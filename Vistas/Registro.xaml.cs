7d0ba463b25463a4ac08fc55a8f4b6a0c0d6f2e3
Master <master@srvuisrael.edu.ec>
2024-01-08 20:40:04 -0500

    Ventana Resumen

--- Vistas/Registro.xaml.cs
+++ Vistas/Registro.xaml.cs
@@ -1,8 +1,10 @@
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
 namespace gMunozExamen.Vistas;
 
 public partial class Registro : ContentPage
 {
-	public Registro(String usuario)
+	public Registro(string usuario)
 	{
 		InitializeComponent();
 		lblUsuario.Text = "Usuario Conectado: " + usuario;
@@ -12,13 +14,43 @@ public partial class Registro : ContentPage
     {
         double pagoTotal = 1500.00;
         double pagoInicial = Convert.ToDouble(txtMonto.Text);
-        double cuota = ((pagoTotal -pagoInicial)/4)+0.4*(pagoTotal);
+        double cuota = ((pagoTotal -pagoInicial)/4)+(0.04*pagoTotal);
         txtPagoMensual.Text = cuota.ToString();
 
     }
 
     private void btnResumen_Clicked(object sender, EventArgs e)
     {
+        string usuario = lblUsuario.Text;
+        string nombre = txtNombre.Text;
+        string apellido = txtApellido.Text;
+        string edad = txtEdad.Text;
+        string fecha = dpFecha.Date.ToString();
+        string ciudad = pkCuidad.Items[pkCuidad.SelectedIndex].ToString();
+        string pais = pkPais.Items[pkPais.SelectedIndex].ToString();
+        string montoInicial = txtMonto.Text;
+        string pagoMensual = txtPagoMensual.Text;
+        double pagoTotal = Convert.ToDouble(montoInicial) +Convert.ToDouble(txtPagoMensual.Text)*4;
 
+        DisplayAlert("RESUMEN", usuario+"\nNombre: " +nombre+"\nApellido: "+apellido+"\nEdad: "+edad+"\nFecha: "+fecha+"\nCuidad: "+ciudad+"\nPais: "+pais+"\nMonto Inicial: "+montoInicial+"\nPagoMensual: "+pagoMensual+"\nPagoTotal: "+pagoTotal, "CERRAR");
+
+    }
+
+    private void txtMonto_TextChanged(object sender, TextChangedEventArgs e)
+    {
+        try
+        {
+            double monto = Convert.ToDouble(txtMonto.Text);
+            if(monto > 1500 || monto <=0)
+            {
+                DisplayAlert("ALERTA", "Solo valores mayores a 0 y menores a 1500", "Cancel");
+
+            }
+        }
+        catch (Exception)
+        {
+
+            throw;
+        }
     }
 }
